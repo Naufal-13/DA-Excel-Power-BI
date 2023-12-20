8912f166631f649fcad36aa551678b220428d059
Naufal Ibadurrahman <naufalib13@gmail.com>
2023-12-20 11:09:11 +0700

    Update Pizza SQLQuery.sql

--- Power BI/Power BI & SQL/Pizza Sales Report/Pizza SQLQuery.sql
+++ Power BI/Power BI & SQL/Pizza Sales Report/Pizza SQLQuery.sql
@@ -1,6 +1,9 @@
 
 select * from pizza_sales;
 
+
+### KPI REQUIREMENT ###
+
 # Total_Revenue
 select	SUM(total_price) as Total_Revenue from pizza_sales ;
 
@@ -18,3 +21,18 @@ select CAST( CAST (SUM(quantity) AS DECIMAL(10,2)) /
 CAST (COUNT(distinct order_id) AS DECIMAL(10,2)) AS DECIMAL (10,2))
 as Avg_Pizzas_per_order 
 from pizza_sales;
+
+
+### CHARTS REQUIREMENT ###
+
+# Daily Trend for Total Orders
+SELECT DATENAME(DW, order_date) as order_date, COUNT(DISTINCT order_date) as Total_orders 
+from pizza_sales
+GROUP BY DATENAME(DW, order_date);
+
+# Monthly Trend for Orders
+SELECT DATENAME(MONTH, order_date) as Month_Name, COUNT (DISTINCT order_id) as Total_orders
+from pizza_sales
+GROUP BY DATENAME(MONTH, order_date)
+
+# Percen of Sales by Pizza Category
